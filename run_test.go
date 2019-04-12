6ea6fa3394f7efadeb4c52524268e3e2e4fdd7b1
itchyny <itchyny@hatena.ne.jp>
2019-04-12 14:36:30 +0900

    add tests for Run

---
+++ run_test.go
@@ -0,0 +1,54 @@
+package gojq
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRun(t *testing.T) {
+	testCases := []struct {
+		name     string
+		query    string
+		input    interface{}
+		expected interface{}
+		err      string
+	}{
+		{
+			name:     "number",
+			query:    `.`,
+			input:    128,
+			expected: 128,
+		},
+		{
+			name:     "string",
+			query:    `.`,
+			input:    "foo",
+			expected: "foo",
+		},
+		{
+			name:     "object",
+			query:    `.`,
+			input:    map[string]interface{}{"foo": 128},
+			expected: map[string]interface{}{"foo": 128},
+		},
+		{
+			name:     "array",
+			query:    `.`,
+			input:    []interface{}{"foo", 128},
+			expected: []interface{}{"foo", 128},
+		},
+	}
+
+	for _, tc := range testCases {
+		parser := NewParser()
+		t.Run(tc.name, func(t *testing.T) {
+			query, err := parser.Parse(tc.query)
+			require.NoError(t, err)
+			got, err := Run(query, tc.input)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
