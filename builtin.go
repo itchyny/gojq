b1087ae2808470520e778bdf1717146b2503e836
itchyny <itchyny@hatena.ne.jp>
2019-04-24 12:56:12 +0900

    tweak spaces in until definition

--- builtin.go
+++ builtin.go
@@ -13,6 +13,6 @@ var builtinFuncs = map[string]string{
 			_while;`,
 	"until": `
 		def until(cond; next):
-			def _until: if cond then . else (next|_until) end;
+			def _until: if cond then . else (next | _until) end;
 			_until;`,
 }
