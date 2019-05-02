9e7299ae64e0596e760f21fb18fa6f12ecd6bd79
itchyny <itchyny@hatena.ne.jp>
2019-05-02 17:48:23 +0900

    implement arrays, objects, iterables, booleans, numbers, strings, nulls, values, scalars functions

--- builtin.go
+++ builtin.go
@@ -15,4 +15,13 @@ var builtinFuncs = map[string]string{
 		def until(cond; next):
 			def _until: if cond then . else (next | _until) end;
 			_until;`,
+	"arrays":    `def arrays: select(type == "array");`,
+	"objects":   `def objects: select(type == "object");`,
+	"iterables": `def iterables: select(type |. == "array" or . == "object");`,
+	"booleans":  `def booleans: select(type == "boolean");`,
+	"numbers":   `def numbers: select(type == "number");`,
+	"strings":   `def strings: select(type == "string");`,
+	"nulls":     `def nulls: select(. == null);`,
+	"values":    `def values: select(. != null);`,
+	"scalars":   `def scalars: select(type |. != "array" and . != "object");`,
 }
