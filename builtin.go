b13201646e061c71c739e1bcb6910f7c35bbcbd8
itchyny <itchyny@hatena.ne.jp>
2019-05-06 18:24:31 +0900

    implement ascii_upcase function

--- builtin.go
+++ builtin.go
@@ -76,4 +76,7 @@ var builtinFuncs = map[string]string{
 	"ascii_downcase": `
 		def ascii_downcase:
 			explode | map(if 65 <= . and . <= 90 then . + 32 end) | implode;`,
+	"ascii_upcase": `
+		def ascii_upcase:
+			explode | map(if 97 <= . and . <= 122 then . - 32  end) | implode;`,
 }
