15fe8c69dd2778afe3babc5f6826b5284e2b0ad4
itchyny <itchyny@hatena.ne.jp>
2019-06-21 22:50:52 +0900

    name all the query structs

--- query.go
+++ query.go
@@ -30,85 +30,103 @@ type Comma struct {
 
 // Alt ...
 type Alt struct {
-	Left  *Expr `@@`
-	Right []struct {
-		Op    Operator `@"//"`
-		Right *Expr    `@@`
-	} `@@*`
+	Left  *Expr      `@@`
+	Right []AltRight `@@*`
+}
+
+// AltRight ...
+type AltRight struct {
+	Op    Operator `@"//"`
+	Right *Expr    `@@`
 }
 
 // Expr ...
 type Expr struct {
-	Logic   *Logic   `( @@`
-	If      *If      `| @@`
-	Try     *Try     `| @@`
-	Reduce  *Reduce  `| @@`
-	Foreach *Foreach `| @@ )`
-	Bind    *struct {
-		Pattern *Pattern `"as" @@`
-		Body    *Pipe    `"|" @@`
-	} `@@?`
-	Label *Label `| @@`
+	Logic   *Logic    `( @@`
+	If      *If       `| @@`
+	Try     *Try      `| @@`
+	Reduce  *Reduce   `| @@`
+	Foreach *Foreach  `| @@ )`
+	Bind    *ExprBind `@@?`
+	Label   *Label    `| @@`
+}
+
+// ExprBind ...
+type ExprBind struct {
+	Pattern *Pattern `"as" @@`
+	Body    *Pipe    `"|" @@`
 }
 
 // Logic ...
 type Logic struct {
-	Left  *AndExpr `@@`
-	Right []struct {
-		Op    Operator `@"or"`
-		Right *AndExpr `@@`
-	} `@@*`
+	Left  *AndExpr     `@@`
+	Right []LogicRight `@@*`
+}
+
+// LogicRight ...
+type LogicRight struct {
+	Op    Operator `@"or"`
+	Right *AndExpr `@@`
 }
 
 // AndExpr ...
 type AndExpr struct {
-	Left  *Compare `@@`
-	Right []struct {
-		Op    Operator `@"and"`
-		Right *Compare `@@`
-	} `@@*`
+	Left  *Compare       `@@`
+	Right []AndExprRight `@@*`
+}
+
+// AndExprRight ...
+type AndExprRight struct {
+	Op    Operator `@"and"`
+	Right *Compare `@@`
 }
 
 // Compare ...
 type Compare struct {
-	Left  *Arith `@@`
-	Right *struct {
-		Op    Operator `@CompareOp`
-		Right *Arith   `@@`
-	} `@@?`
+	Left  *Arith        `@@`
+	Right *CompareRight `@@?`
+}
+
+// CompareRight ...
+type CompareRight struct {
+	Op    Operator `@CompareOp`
+	Right *Arith   `@@`
 }
 
 // Arith ...
 type Arith struct {
-	Left  *Factor `( @@`
-	Right []struct {
-		Op    Operator `@("+" | "-")`
-		Right *Factor  `@@`
-	} `@@* )`
+	Left  *Factor      `( @@`
+	Right []ArithRight `@@* )`
+}
+
+// ArithRight ...
+type ArithRight struct {
+	Op    Operator `@("+" | "-")`
+	Right *Factor  `@@`
 }
 
 // Factor ...
 type Factor struct {
-	Left  *Term `@@`
-	Right []struct {
-		Op    Operator `@("*" | "/" | "%")`
-		Right *Term    `@@`
-	} `@@*`
+	Left  *Term         `@@`
+	Right []FactorRight `@@*`
+}
+
+// FactorRight ...
+type FactorRight struct {
+	Op    Operator `@("*" | "/" | "%")`
+	Right *Term    `@@`
 }
 
 // Term ...
 type Term struct {
-	Index    *Index   `( @@`
-	Identity bool     `| @"."`
-	Recurse  bool     `| @".."`
-	Func     *Func    `| @@`
-	Object   *Object  `| @@`
-	Array    *Array   `| @@`
-	Number   *float64 `| @Number`
-	Unary    *struct {
-		Op   Operator `@("+" | "-")`
-		Term *Term    `@@`
-	} `| @@`
+	Index      *Index    `( @@`
+	Identity   bool      `| @"."`
+	Recurse    bool      `| @".."`
+	Func       *Func     `| @@`
+	Object     *Object   `| @@`
+	Array      *Array    `| @@`
+	Number     *float64  `| @Number`
+	Unary      *Unary    `| @@`
 	String     *string   `| @String`
 	Null       bool      `| @"null"`
 	True       bool      `| @"true"`
@@ -118,16 +136,25 @@ type Term struct {
 	SuffixList []*Suffix `@@*`
 }
 
+// Unary ...
+type Unary struct {
+	Op   Operator `@("+" | "-")`
+	Term *Term    `@@`
+}
+
 // Pattern ...
 type Pattern struct {
-	Name   string     `( @Ident`
-	Array  []*Pattern `| "[" @@ ("," @@)* "]"`
-	Object []struct {
-		Key       string   `( ( @Ident | @Keyword )`
-		KeyString string   `| @String ) ":"`
-		Val       *Pattern `@@`
-		KeyOnly   string   `| @Ident`
-	} `| "{" @@ ("," @@)* "}" )`
+	Name   string          `( @Ident`
+	Array  []*Pattern      `| "[" @@ ("," @@)* "]"`
+	Object []PatternObject `| "{" @@ ("," @@)* "}" )`
+}
+
+// PatternObject ...
+type PatternObject struct {
+	Key       string   `( ( @Ident | @Keyword )`
+	KeyString string   `| @String ) ":"`
+	Val       *Pattern `@@`
+	KeyOnly   string   `| @Ident`
 }
 
 // Index ...
@@ -147,14 +174,17 @@ type Func struct {
 
 // Object ...
 type Object struct {
-	KeyVals []struct {
-		Key           string  `( ( ( @Ident | @Keyword )`
-		KeyString     *string `  | @String )`
-		Pipe          *Pipe   `| "(" @@ ")" ) ":"`
-		Val           *Expr   `@@`
-		KeyOnly       *string `| @Ident`
-		KeyOnlyString *string `| @String`
-	} `"{" (@@ ("," @@)*)? "}"`
+	KeyVals []ObjectKeyVal `"{" (@@ ("," @@)*)? "}"`
+}
+
+// ObjectKeyVal ...
+type ObjectKeyVal struct {
+	Key           string  `( ( ( @Ident | @Keyword )`
+	KeyString     *string `  | @String )`
+	Pipe          *Pipe   `| "(" @@ ")" ) ":"`
+	Val           *Expr   `@@`
+	KeyOnly       *string `| @Ident`
+	KeyOnlyString *string `| @String`
 }
 
 // Array ...
@@ -179,13 +209,16 @@ type SuffixIndex struct {
 
 // If ...
 type If struct {
-	Cond *Pipe `"if" @@`
+	Cond *Pipe    `"if" @@`
+	Then *Pipe    `"then" @@`
+	Elif []IfElif `@@*`
+	Else *Pipe    `("else" @@)? "end"`
+}
+
+// IfElif ...
+type IfElif struct {
+	Cond *Pipe `"elif" @@`
 	Then *Pipe `"then" @@`
-	Elif []struct {
-		Cond *Pipe `"elif" @@`
-		Then *Pipe `"then" @@`
-	} `@@*`
-	Else *Pipe `("else" @@)? "end"`
 }
 
 // Try ...
