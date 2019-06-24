0bef9578d0196aa674ded9902f14c2c188779628
itchyny <itchyny@hatena.ne.jp>
2019-06-24 21:51:50 +0900

    implement Stringer for opcode

--- code.go
+++ code.go
@@ -14,3 +14,20 @@ const (
 	opjump
 	opret
 )
+
+func (op opcode) String() string {
+	switch op {
+	case opload:
+		return "load"
+	case opconst:
+		return "const"
+	case opfork:
+		return "fork"
+	case opjump:
+		return "jump"
+	case opret:
+		return "ret"
+	default:
+		panic(op)
+	}
+}
