8e286266d48d2fbe17b918816f0f2fb0e5fb4f0f
itchyny <itchyny@hatena.ne.jp>
2019-07-03 13:36:41 +0900

    implement Stringer of Query

--- query.go
+++ query.go
@@ -1,9 +1,31 @@
 package gojq
 
+import (
+	"fmt"
+	"strings"
+)
+
 // Query ...
 type Query struct {
 	FuncDefs []*FuncDef `@@*`
 	Pipe     *Pipe      `@@?`
+}
+
+func (q *Query) String() string {
+	var s strings.Builder
+	for i, fd := range q.FuncDefs {
+		if i > 0 {
+			s.WriteByte(' ')
+		}
+		fmt.Fprint(&s, fd)
+	}
+	if q.Pipe != nil {
+		if len(q.FuncDefs) > 0 {
+			s.WriteByte(' ')
+		}
+		fmt.Fprint(&s, q.Pipe)
+	}
+	return s.String()
 }
 
 // Run query.
@@ -21,14 +43,53 @@ type FuncDef struct {
 	Body *Query   `@@ ";"`
 }
 
+func (e *FuncDef) String() string {
+	var s strings.Builder
+	fmt.Fprintf(&s, "def %s", e.Name)
+	if len(e.Args) > 0 {
+		s.WriteByte('(')
+		for i, e := range e.Args {
+			if i > 0 {
+				s.WriteString("; ")
+			}
+			fmt.Fprint(&s, e)
+		}
+		s.WriteByte(')')
+	}
+	fmt.Fprintf(&s, ": %s;", e.Body)
+	return s.String()
+}
+
 // Pipe ...
 type Pipe struct {
 	Commas []*Comma `@@ ("|" @@)*`
 }
 
+func (e *Pipe) String() string {
+	var s strings.Builder
+	for i, e := range e.Commas {
+		if i > 0 {
+			s.WriteString(" | ")
+		}
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // Comma ...
 type Comma struct {
 	Alts []*Alt `@@ ("," @@)*`
+}
+
+func (e *Comma) String() string {
+	var s strings.Builder
+	for i, e := range e.Alts {
+		if i > 0 {
+			s.WriteString(", ")
+		}
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
 }
 
 // Alt ...
@@ -37,10 +98,23 @@ type Alt struct {
 	Right []AltRight `@@*`
 }
 
+func (e *Alt) String() string {
+	var s strings.Builder
+	fmt.Fprint(&s, e.Left)
+	for _, e := range e.Right {
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // AltRight ...
 type AltRight struct {
 	Op    Operator `@"//"`
 	Right *Expr    `@@`
+}
+
+func (e AltRight) String() string {
+	return fmt.Sprintf(" %s %s", e.Op, e.Right)
 }
 
 // Expr ...
@@ -54,10 +128,35 @@ type Expr struct {
 	Label   *Label    `| @@`
 }
 
+func (e *Expr) String() string {
+	var s strings.Builder
+	if e.Logic != nil {
+		fmt.Fprint(&s, e.Logic)
+	} else if e.If != nil {
+		fmt.Fprint(&s, e.If)
+	} else if e.Try != nil {
+		fmt.Fprint(&s, e.Try)
+	} else if e.Reduce != nil {
+		fmt.Fprint(&s, e.Reduce)
+	} else if e.Foreach != nil {
+		fmt.Fprint(&s, e.Foreach)
+	}
+	if e.Bind != nil {
+		fmt.Fprint(&s, e.Bind)
+	} else if e.Label != nil {
+		fmt.Fprint(&s, e.Label)
+	}
+	return s.String()
+}
+
 // ExprBind ...
 type ExprBind struct {
 	Pattern *Pattern `"as" @@`
 	Body    *Pipe    `"|" @@`
+}
+
+func (e *ExprBind) String() string {
+	return fmt.Sprintf(" as %s | %s", e.Pattern, e.Body)
 }
 
 // Logic ...
@@ -66,10 +165,23 @@ type Logic struct {
 	Right []LogicRight `@@*`
 }
 
+func (e *Logic) String() string {
+	var s strings.Builder
+	fmt.Fprint(&s, e.Left)
+	for _, e := range e.Right {
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // LogicRight ...
 type LogicRight struct {
 	Op    Operator `@"or"`
 	Right *AndExpr `@@`
+}
+
+func (e LogicRight) String() string {
+	return fmt.Sprintf(" %s %s", e.Op, e.Right)
 }
 
 // AndExpr ...
@@ -78,10 +190,23 @@ type AndExpr struct {
 	Right []AndExprRight `@@*`
 }
 
+func (e *AndExpr) String() string {
+	var s strings.Builder
+	fmt.Fprint(&s, e.Left)
+	for _, e := range e.Right {
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // AndExprRight ...
 type AndExprRight struct {
 	Op    Operator `@"and"`
 	Right *Compare `@@`
+}
+
+func (e AndExprRight) String() string {
+	return fmt.Sprintf(" %s %s", e.Op, e.Right)
 }
 
 // Compare ...
@@ -90,10 +215,23 @@ type Compare struct {
 	Right *CompareRight `@@?`
 }
 
+func (e *Compare) String() string {
+	var s strings.Builder
+	fmt.Fprint(&s, e.Left)
+	if e.Right != nil {
+		fmt.Fprint(&s, e.Right)
+	}
+	return s.String()
+}
+
 // CompareRight ...
 type CompareRight struct {
 	Op    Operator `@CompareOp`
 	Right *Arith   `@@`
+}
+
+func (e *CompareRight) String() string {
+	return fmt.Sprintf(" %s %s", e.Op, e.Right)
 }
 
 // Arith ...
@@ -102,10 +240,23 @@ type Arith struct {
 	Right []ArithRight `@@* )`
 }
 
+func (e *Arith) String() string {
+	var s strings.Builder
+	fmt.Fprint(&s, e.Left)
+	for _, e := range e.Right {
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // ArithRight ...
 type ArithRight struct {
 	Op    Operator `@("+" | "-")`
 	Right *Factor  `@@`
+}
+
+func (e ArithRight) String() string {
+	return fmt.Sprintf(" %s %s", e.Op, e.Right)
 }
 
 // Factor ...
@@ -114,10 +265,23 @@ type Factor struct {
 	Right []FactorRight `@@*`
 }
 
+func (e *Factor) String() string {
+	var s strings.Builder
+	fmt.Fprint(&s, e.Left)
+	for _, e := range e.Right {
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // FactorRight ...
 type FactorRight struct {
 	Op    Operator `@("*" | "/" | "%")`
 	Right *Term    `@@`
+}
+
+func (e FactorRight) String() string {
+	return fmt.Sprintf(" %s %s", e.Op, e.Right)
 }
 
 // Term ...
@@ -139,10 +303,51 @@ type Term struct {
 	SuffixList []*Suffix `@@*`
 }
 
+func (e *Term) String() string {
+	var s strings.Builder
+	if e.Index != nil {
+		fmt.Fprint(&s, e.Index)
+	} else if e.Identity {
+		s.WriteString(".")
+	} else if e.Recurse {
+		s.WriteString("..")
+	} else if e.Func != nil {
+		fmt.Fprint(&s, e.Func)
+	} else if e.Object != nil {
+		fmt.Fprint(&s, e.Object)
+	} else if e.Array != nil {
+		fmt.Fprint(&s, e.Array)
+	} else if e.Number != nil {
+		fmt.Fprint(&s, *e.Number)
+	} else if e.Unary != nil {
+		fmt.Fprint(&s, e.Unary)
+	} else if e.Str != "" {
+		fmt.Fprint(&s, e.Str)
+	} else if e.Null {
+		s.WriteString("null")
+	} else if e.True {
+		s.WriteString("true")
+	} else if e.False {
+		s.WriteString("false")
+	} else if e.Break != "" {
+		fmt.Fprintf(&s, "break %s", e.Break)
+	} else if e.Pipe != nil {
+		fmt.Fprintf(&s, "(%s)", e.Pipe)
+	}
+	for _, e := range e.SuffixList {
+		fmt.Fprint(&s, e)
+	}
+	return s.String()
+}
+
 // Unary ...
 type Unary struct {
 	Op   Operator `@("+" | "-")`
 	Term *Term    `@@`
+}
+
+func (e *Unary) String() string {
+	return fmt.Sprintf("%s %s", e.Op, e.Term)
 }
 
 // Pattern ...
@@ -152,12 +357,39 @@ type Pattern struct {
 	Object []PatternObject `| "{" @@ ("," @@)* "}" )`
 }
 
+func (e *Pattern) String() string {
+	var s strings.Builder
+	if e.Name != "" {
+		s.WriteString(e.Name)
+	}
+	return s.String()
+}
+
 // PatternObject ...
 type PatternObject struct {
 	Key       string   `( ( @Ident | @Keyword )`
 	KeyString string   `| @String ) ":"`
 	Val       *Pattern `@@`
 	KeyOnly   string   `| @Ident`
+}
+
+func (e *PatternObject) String() string {
+	var s strings.Builder
+	if e.Key != "" {
+		s.WriteString(e.Key)
+		s.WriteString(": ")
+	}
+	if e.KeyString != "" {
+		s.WriteString(e.KeyString)
+		s.WriteString(": ")
+	}
+	if e.Val != nil {
+		fmt.Fprint(&s, e.Val)
+	}
+	if e.KeyOnly != "" {
+		s.WriteString(e.KeyOnly)
+	}
+	return s.String()
 }
 
 // Index ...
@@ -169,6 +401,32 @@ type Index struct {
 	End     *Pipe  `@@? )? | ":" @@ ) "]" )`
 }
 
+func (e *Index) String() string {
+	var s strings.Builder
+	s.WriteByte('.')
+	if e.Name != "" {
+		s.WriteString(e.Name)
+	} else if e.Str != "" {
+		s.WriteString(e.Str)
+	} else {
+		s.WriteByte('[')
+		if e.Start != nil {
+			fmt.Fprint(&s, e.Start)
+			if e.IsSlice {
+				s.WriteByte(':')
+				if e.End != nil {
+					fmt.Fprint(&s, e.End)
+				}
+			}
+		} else if e.End != nil {
+			s.WriteByte(':')
+			fmt.Fprint(&s, e.End)
+		}
+		s.WriteByte(']')
+	}
+	return s.String()
+}
+
 // Func ...
 type Func struct {
 	Name    string  `@Ident`
@@ -176,9 +434,41 @@ type Func struct {
 	funcDef *FuncDef
 }
 
+func (e *Func) String() string {
+	var s strings.Builder
+	s.WriteString(e.Name)
+	if len(e.Args) > 0 {
+		s.WriteByte('(')
+		for i, e := range e.Args {
+			if i > 0 {
+				s.WriteString("; ")
+			}
+			fmt.Fprint(&s, e)
+		}
+		s.WriteByte(')')
+	}
+	return s.String()
+}
+
 // Object ...
 type Object struct {
 	KeyVals []ObjectKeyVal `"{" (@@ ("," @@)*)? "}"`
+}
+
+func (e *Object) String() string {
+	if len(e.KeyVals) == 0 {
+		return "{}"
+	}
+	var s strings.Builder
+	s.WriteString("{ ")
+	for i, kv := range e.KeyVals {
+		if i > 0 {
+			s.WriteString(", ")
+		}
+		fmt.Fprint(&s, &kv)
+	}
+	s.WriteString(" }")
+	return s.String()
 }
 
 // ObjectKeyVal ...
@@ -191,9 +481,37 @@ type ObjectKeyVal struct {
 	KeyOnlyString string  `| @String`
 }
 
+func (e *ObjectKeyVal) String() string {
+	var s strings.Builder
+	if e.Key != "" {
+		s.WriteString(e.Key)
+	} else if e.KeyString != "" {
+		s.WriteString(e.KeyString)
+	} else if e.Pipe != nil {
+		fmt.Fprintf(&s, "(%s)", e.Pipe)
+	}
+	if e.Val != nil {
+		fmt.Fprintf(&s, ": %s", e.Val)
+	}
+	if e.KeyOnly != nil {
+		s.WriteString(*e.KeyOnly)
+	}
+	if e.KeyOnlyString != "" {
+		s.WriteString(e.KeyOnlyString)
+	}
+	return s.String()
+}
+
 // Array ...
 type Array struct {
 	Pipe *Pipe `"[" @@? "]"`
+}
+
+func (e *Array) String() string {
+	if e.Pipe == nil {
+		return "[]"
+	}
+	return fmt.Sprintf("[%s]", e.Pipe)
 }
 
 // Suffix ...
@@ -204,11 +522,33 @@ type Suffix struct {
 	Optional    bool         `| @"?"`
 }
 
+func (e *Suffix) String() string {
+	var s strings.Builder
+	if e.Index != nil {
+		fmt.Fprint(&s, e.Index)
+	} else if e.SuffixIndex != nil {
+		fmt.Fprint(&s, e.SuffixIndex)
+	} else if e.Iter {
+		s.WriteString("[]")
+	} else if e.Optional {
+		s.WriteString("?")
+	}
+	return s.String()
+}
+
 // SuffixIndex ...
 type SuffixIndex struct {
 	Start   *Pipe `"[" ( @@`
 	IsSlice bool  `( @":"`
 	End     *Pipe `@@? )? | ":" @@ ) "]"`
+}
+
+func (e *SuffixIndex) String() string {
+	return (&Index{
+		Start:   e.Start,
+		IsSlice: e.IsSlice,
+		End:     e.End,
+	}).String()[1:]
 }
 
 // If ...
@@ -219,10 +559,27 @@ type If struct {
 	Else *Pipe    `("else" @@)? "end"`
 }
 
+func (e *If) String() string {
+	var s strings.Builder
+	fmt.Fprintf(&s, "if %s then %s", e.Cond, e.Then)
+	for _, e := range e.Elif {
+		fmt.Fprintf(&s, " %s", &e)
+	}
+	if e.Else != nil {
+		fmt.Fprintf(&s, " else %s", e.Else)
+	}
+	s.WriteString(" end")
+	return s.String()
+}
+
 // IfElif ...
 type IfElif struct {
 	Cond *Pipe `"elif" @@`
 	Then *Pipe `"then" @@`
+}
+
+func (e *IfElif) String() string {
+	return fmt.Sprintf("elif %s then %s", e.Cond, e.Then)
 }
 
 // Try ...
@@ -231,12 +588,25 @@ type Try struct {
 	Catch *Pipe `("catch" @@)?`
 }
 
+func (e *Try) String() string {
+	var s strings.Builder
+	fmt.Fprintf(&s, "try %s", e.Body)
+	if e.Catch != nil {
+		fmt.Fprintf(&s, " catch %s", e.Catch)
+	}
+	return s.String()
+}
+
 // Reduce ...
 type Reduce struct {
 	Term    *Term    `"reduce" @@`
 	Pattern *Pattern `"as" @@`
 	Start   *Pipe    `"(" @@`
 	Update  *Pipe    `";" @@ ")"`
+}
+
+func (e *Reduce) String() string {
+	return fmt.Sprintf("reduce %s as %s (%s; %s)", e.Term, e.Pattern, e.Start, e.Update)
 }
 
 // Foreach ...
@@ -248,8 +618,22 @@ type Foreach struct {
 	Extract *Pipe    `(";" @@)? ")"`
 }
 
+func (e *Foreach) String() string {
+	var s strings.Builder
+	fmt.Fprintf(&s, "foreach %s as %s (%s; %s", e.Term, e.Pattern, e.Start, e.Update)
+	if e.Extract != nil {
+		fmt.Fprintf(&s, "; %s", e.Extract)
+	}
+	s.WriteByte(')')
+	return s.String()
+}
+
 // Label ...
 type Label struct {
 	Ident string `"label" @Ident`
 	Body  *Pipe  `"|" @@`
+}
+
+func (e *Label) String() string {
+	return fmt.Sprintf("label %s | %s", e.Ident, e.Body)
 }
