b4b1a39c2a4d447c6ce5f2023634d5c15f6f18ff
itchyny <itchyny@hatena.ne.jp>
2019-07-21 22:38:01 +0900

    implement bsearch function

--- builtin.go
+++ builtin.go
@@ -198,4 +198,32 @@ var BuiltinFuncDefinitions = map[string]string{
 				| select(length > 0);
 		def paths(f):
 			. as $x | paths | select(. as $p | $x | getpath($p) | f);`,
+	"bsearch": `
+		def bsearch($target):
+			if length == 0 then
+				-1
+			elif length == 1 then
+				if $target == .[0] then 0 elif $target < .[0] then -1 else -2 end
+			else . as $in
+				# state variable: [start, end, answer]
+				# where start and end are the upper and lower offsets to use.
+				| [0, length-1, null]
+				| until(.[0] > .[1];
+					if .[2] != null then (.[1] = -1) # i.e. break
+					else
+						(((.[1] + .[0]) / 2) | floor) as $mid
+						| $in[$mid] as $monkey
+						| if $monkey == $target then (.[2] = $mid) # success
+							elif .[0] == .[1] then (.[1] = -1) # failure
+							elif $monkey < $target then (.[0] = ($mid + 1))
+							else (.[1] = ($mid - 1))
+							end
+					end)
+				| if .[2] == null then # compute the insertion point
+						if $in[.[0]] < $target then (-2 -.[0])
+						else (-1 -.[0])
+						end
+					else .[2]
+					end
+			end;`,
 }
