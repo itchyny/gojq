8b529163caeab1b12a9cbd235c607f889d761f5d
Joris Melchior <joris.melchior@gmail.com>
2019-11-11 20:01:16 -0500

    Fix lint error

--- cli/run_lib.go
+++ cli/run_lib.go
@@ -4,6 +4,7 @@ import (
 	"io"
 )
 
+// RunLib allows use as library where caller sets streams and params programmatically
 func RunLib(inStream io.Reader, outStream io.Writer, errStream io.Writer, opts []string) int {
 	return (&cli{
 		inStream:  inStream,
