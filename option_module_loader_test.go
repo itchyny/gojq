56ed44d277fd91cffd5720ddb9aa1d60a4a9c01f
itchyny <itchyny@hatena.ne.jp>
2020-04-01 13:43:04 +0900

    move the example of WithModuleLoader compiler option

---
+++ option_module_loader_test.go
@@ -0,0 +1,58 @@
+package gojq_test
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/itchyny/gojq"
+)
+
+type moduleLoader struct{}
+
+func (*moduleLoader) LoadModule(name string) (*gojq.Module, error) {
+	switch name {
+	case "module1":
+		return gojq.ParseModule(`
+			module { name: "module1", test: 42 };
+			import "module2" as foo;
+			def f: foo::f;
+		`)
+	case "module2":
+		return gojq.ParseModule(`
+			def f: .foo;
+		`)
+	}
+	return nil, fmt.Errorf("module not found: %q", name)
+}
+
+func ExampleWithModuleLoader() {
+	query, err := gojq.Parse(`
+		import "module1" as m;
+		m::f
+	`)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	code, err := gojq.Compile(
+		query,
+		gojq.WithModuleLoader(&moduleLoader{}),
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input := map[string]interface{}{"foo": 42}
+	iter := code.Run(input)
+	for {
+		v, ok := iter.Next()
+		if !ok {
+			break
+		}
+		if err, ok := v.(error); ok {
+			log.Fatalln(err)
+		}
+		fmt.Printf("%#v\n", v)
+	}
+
+	// Output:
+	// 42
+}
