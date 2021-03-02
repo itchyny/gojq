d2f80a2a02d81c245c3da68e0feb253d75261b28
itchyny <itchyny@hatena.ne.jp>
2021-03-02 19:40:19 +0900

    fix builtin to built-in in the comment of ValueError

--- error.go
+++ error.go
@@ -9,7 +9,7 @@ import (
 
 // ValueError is an interface for errors with a value for internal function.
 // Return an error implementing this interface when you want to catch error
-// values (not error messages) by try-catch, just like builtin error function.
+// values (not error messages) by try-catch, just like built-in error function.
 // Refer to WithFunction to add a custom internal function.
 type ValueError interface {
 	error
