c457f97b1600728a224188d5a81cae943488eae8
itchyny <itchyny@hatena.ne.jp>
2021-05-16 02:59:21 +0900

    export NewIter function to create a new Iter from values

--- iter.go
+++ iter.go
@@ -21,3 +21,20 @@ func (iter *unitIter) Next() (interface{}, bool) {
 	iter.done = true
 	return iter.v, true
 }
+
+// NewIter creates a new Iter from values.
+func NewIter(values ...interface{}) Iter {
+	iter := sliceIter(values)
+	return &iter
+}
+
+type sliceIter []interface{}
+
+func (iter *sliceIter) Next() (interface{}, bool) {
+	if len(*iter) == 0 {
+		return nil, false
+	}
+	value := (*iter)[0]
+	*iter = (*iter)[1:]
+	return value, true
+}
