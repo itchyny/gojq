4ada50f9f776f511a3c161fe51ab7a6d91386904
itchyny <itchyny@hatena.ne.jp>
2021-09-28 06:27:35 +0900

    fix style in limited writer

--- error.go
+++ error.go
@@ -362,8 +362,7 @@ func (w *limitedWriter) Write(bs []byte) (int, error) {
 
 func (w *limitedWriter) WriteByte(b byte) error {
 	w.buf[w.off] = b
-	w.off++
-	if w.off == len(w.buf) {
+	if w.off++; w.off == len(w.buf) {
 		panic(nil)
 	}
 	return nil
